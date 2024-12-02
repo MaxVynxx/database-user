58929a2a3c56e6dfea4e4f4adfab6ff534fd284d
MaxVynxx <arifinstore91@gmail.com>
2024-12-02 20:17:49 +0700

    Add files via upload

---
+++ datanya.js
@@ -0,0 +1,38 @@
+{
+    "users": [
+        {
+            "username": "user1",
+            "password": "pass1",
+            "role": "user"
+        },
+        {
+            "username": "arifin",
+            "password": "manz",
+            "role": "admin"
+        }, 
+        
+        {
+            "username": "tynzy",
+            "password": "1",
+            "role": "user"
+        }, 
+        
+        {
+            "username": "dika",
+            "password": "1",
+            "role": "user"
+        }, 
+        
+        {
+            "username": "rayhan",
+            "password": "1",
+            "role": "user"
+        }, 
+        
+        {
+            "username": "lil",
+            "password": "1",
+            "role": "user"
+        }
+    ]
+}
